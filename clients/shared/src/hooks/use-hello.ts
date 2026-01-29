04ba53791b036723fd02a8a0d8ac30f596b7e6aa
Manuel Torres <torres.man@northeastern.edu>
2026-01-28 21:10:08 -0500

    small

--- clients/shared/src/hooks/use-hello.ts
+++ clients/shared/src/hooks/use-hello.ts
@@ -4,3 +4,4 @@ export const createHelloService = (api: HttpClient) => ({
   getHello: () => api.get<string>('/api/v1/hello'),
   getHelloName: (name: string) => api.get<string>(`/api/v1/hello/${name}`),
 })
+
