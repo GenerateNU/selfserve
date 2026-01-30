fdcf01dc1f98eac954c99d3550e04fd903059742
oilagh <tdk737@gmail.com>
2026-01-30 06:18:52 -0500

    fix requests endpoint

--- backend/internal/service/server.go
+++ backend/internal/service/server.go
@@ -83,7 +83,7 @@ func setupRoutes(app *fiber.App, repo *storage.Repository) {
 	// Request routes
 	api.Route("/request", func(r fiber.Router) {
 		r.Post("/", reqsHandler.CreateRequest)
-		r.Post("/:id", reqsHandler.FindRequest)
+		r.Get("/:id", reqsHandler.FindRequest)
 	})
 
 	// Hotel routes
