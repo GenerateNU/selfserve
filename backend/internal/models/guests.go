05e2e0249c44402d2a27bac6c5a4a09dc8347980
Zaydaan Jahangir <jahangir.z@northeastern.edu>
2026-02-07 22:19:47 -0500

    fix: Remove omitempty on required fields

--- backend/internal/models/guests.go
+++ backend/internal/models/guests.go
@@ -10,8 +10,8 @@ type CreateGuest struct {
 }
 
 type UpdateGuest struct {
-	FirstName      string  `json:"first_name,omitempty" validate:"omitempty,notblank" example:"Jane"`
-	LastName       string  `json:"last_name,omitempty" validate:"omitempty,notblank" example:"Doe"`
+	FirstName      string  `json:"first_name" validate:"notblank" example:"Jane"`
+	LastName       string  `json:"last_name" validate:"notblank" example:"Doe"`
 	ProfilePicture *string `json:"profile_picture,omitempty" validate:"omitempty,url" example:"https://example.com/john.jpg"`
 	Timezone       *string `json:"timezone,omitempty" validate:"omitempty,timezone" example:"America/New_York"`
 }
