fc2af7e77f3900e3816060428ec6a2e1b169bce5
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-02-19 17:18:22 -0500

    fallback

--- clients/web/src/routes/sign-in.tsx
+++ clients/web/src/routes/sign-in.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from '@tanstack/react-router'
 import { SignIn } from '@clerk/clerk-react'
 
 export const Route = createFileRoute('/sign-in')({
-  component: () => <SignIn forceRedirectUrl="/home" />,
+  component: () => <SignIn />,
 })
