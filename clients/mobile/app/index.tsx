89766cabd38ca4a4efc1ac8f365d25ac693c33b4
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2026-03-14 13:43:04 -0400

    Update index.tsx

--- clients/mobile/app/index.tsx
+++ clients/mobile/app/index.tsx
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <View className="flex-1 items-center p-6">
       <View className="flex-1 justify-center max-w-4xl mx-auto">
+        {/* Hello world text */}
         <Text className="text-6xl font-bold">Hello</Text>
         <Text className="text-4xl text-gray-700">World</Text>
       </View>
