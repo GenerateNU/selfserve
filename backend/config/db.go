b646e6e3b684af053c5ec77cb1730b4399b910ca
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-03-14 01:27:22 -0400

    more configurations for our db config

--- backend/config/db.go
+++ backend/config/db.go
@@ -12,8 +12,11 @@ type DB struct {
 	Password        string        `env:"PASSWORD, required"`             // db password to connect with
 	Name            string        `env:"NAME, required"`                 // db name to connect to
 	SSLMode         string        `env:"SSLMODE, default=disable"`       // sslmode for connection (disable for local, require for production)
-	MaxConns        int32         `env:"MAX_CONNS, default=8"`           // max number of connections to the database
-	MaxConnLifetime time.Duration `env:"MAX_CONN_LIFETIME, default=30s"` // max lifetime of a connection before automatically closing
+	MaxConns          int32         `env:"MAX_CONNS, default=8"`                // max number of connections to the database
+	MinConns          int32         `env:"MIN_CONNS, default=2"`                // min number of idle connections to keep open
+	MaxConnLifetime   time.Duration `env:"MAX_CONN_LIFETIME, default=30s"`      // max lifetime of a connection before automatically closing
+	MaxConnIdleTime   time.Duration `env:"MAX_CONN_IDLE_TIME, default=5m"`      // max time a connection can be idle before closing
+	HealthCheckPeriod time.Duration `env:"HEALTH_CHECK_PERIOD, default=1m"`     // how often to health-check idle connections
 
 }
 
