11313c9e4d7101a0c571329c914e1e76b61b8402
eric-kitagawa <kitagawa.e@northeastern.edu>
2026-03-13 20:17:01 -0400

    feat: get room floors

---
+++ backend/internal/models/rooms.go
@@ -0,0 +1,21 @@
+package models
+
+type Room struct {
+	RoomNumber int    `json:"room_number"`
+	Floor      int    `json:"floor"`
+	SuiteType  string `json:"suite_type"`
+	RoomStatus string `json:"room_status"`
+} //@name Room
+
+type RoomFilter struct {
+	Floors *[]int `query:"floors"`
+	Cursor string `query:"cursor"`
+	Limit  int    `query:"limit"`
+}
+
+// Read model for rooms page on the frontend
+type RoomWithOptionalGuestBooking struct {
+	Room
+	Guests        []Guest       `json:"guests"`
+	BookingStatus BookingStatus `json:"booking_status"`
+} //@name RoomWithOptionalGuestBooking
