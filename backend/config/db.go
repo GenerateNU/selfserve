3bbe64f63ec317d2831e0f339edae7cffcc91a72
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-03-14 01:30:58 -0400

    update our idle + health check timeout to be similarly restrictive

--- backend/config/db.go
+++ backend/config/db.go
@@ -15,8 +15,8 @@ type DB struct {
 	MaxConns          int32         `env:"MAX_CONNS, default=8"`                // max number of connections to the database
 	MinConns          int32         `env:"MIN_CONNS, default=2"`                // min number of idle connections to keep open
 	MaxConnLifetime   time.Duration `env:"MAX_CONN_LIFETIME, default=30s"`      // max lifetime of a connection before automatically closing
-	MaxConnIdleTime   time.Duration `env:"MAX_CONN_IDLE_TIME, default=5m"`      // max time a connection can be idle before closing
-	HealthCheckPeriod time.Duration `env:"HEALTH_CHECK_PERIOD, default=1m"`     // how often to health-check idle connections
+	MaxConnIdleTime   time.Duration `env:"MAX_CONN_IDLE_TIME, default=15s"`     // max time a connection can be idle before closing
+	HealthCheckPeriod time.Duration `env:"HEALTH_CHECK_PERIOD, default=30s"`    // how often to health-check idle connections
 
 }
 
