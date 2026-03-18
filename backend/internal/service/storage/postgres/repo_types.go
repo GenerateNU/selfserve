aa6f944172b2145f2565c4c88c30068c2c59c071
Manuel Torres <torres.man@northeastern.edu>
2026-03-17 20:43:05 -0400

    Feat/guest and booking endpoint (#178)
    * rooms migration file
    
    * feat: guest booking migration
    
    * rls fix
    
    * indexes on both tables
    
    * type fix
    
    * incomplete guest bookings table + endpoint
    
    * server integrated + openapi
    
    * moved getRooms to rooms repo
    
    * get guests endpoint
    
    * changes
    
    * fixed openapi
    
    * remove stale webhook dir
    
    * route for specific guest info
    
    * cleanup GetBookingsByFloor handler
    
    * changed some models
    
    * filtered by hotel id
    
    * lint
    
    * may the lint gods be merciful
    
    * comments
    
    * fixed issue
    
    * rename
    
    ---------
    
    Co-authored-by: eric-kitagawa <kitagawa.e@northeastern.edu>

--- backend/internal/service/storage/postgres/repo_types.go
+++ backend/internal/service/storage/postgres/repo_types.go
@@ -15,6 +15,8 @@ type GuestsRepository interface {
 	InsertGuest(ctx context.Context, guest *models.CreateGuest) (*models.Guest, error)
 	FindGuest(ctx context.Context, id string) (*models.Guest, error)
 	UpdateGuest(ctx context.Context, id string, update *models.UpdateGuest) (*models.Guest, error)
+	FindGuestsWithActiveBooking(ctx context.Context, filters *models.GuestFilters) ([]*models.GuestWithBooking, error)
+	FindGuestWithStayHistory(ctx context.Context, id string) (*models.GuestWithStays, error)
 }
 
 type RequestsRepository interface {
