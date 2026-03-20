d88dfaea5d9ac45b8f509bd5fa7404a94e8cf3ba
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-03-20 02:48:07 -0400

    test

--- clients/web/src/routes/_protected.tsx
+++ clients/web/src/routes/_protected.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { SideBarWithContent } from "@/components/SideBarWithContent";
-
+// test
 export const Route = createFileRoute("/_protected")({
   component: () => (
     <>
