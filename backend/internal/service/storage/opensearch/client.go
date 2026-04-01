47812b81642ab93ee577d8c02828dec3b5069beb
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-03-31 20:56:45 -0400

    move index schema to indices.go

--- backend/internal/service/storage/opensearch/client.go
+++ backend/internal/service/storage/opensearch/client.go
@@ -31,46 +31,6 @@ func NewClient(cfg config.OpenSearch) (*opensearch.Client, error) {
 	return client, nil
 }
 
-// guestsIndexMapping defines the index schema. Fields are denormalized from
-// guests + guest_bookings + rooms so all filtering can happen in one query.
-var guestsIndexMapping = map[string]interface{}{
-	"mappings": map[string]interface{}{
-		"properties": map[string]interface{}{
-			"id":       map[string]string{"type": "keyword"},
-			"hotel_id": map[string]string{"type": "keyword"},
-			"full_name": map[string]interface{}{
-				"type": "text",
-				"fields": map[string]interface{}{
-					"keyword": map[string]string{"type": "keyword"},
-				},
-			},
-			"first_name": map[string]interface{}{
-				"type": "text",
-				"fields": map[string]interface{}{
-					"keyword": map[string]string{"type": "keyword"},
-				},
-			},
-			"last_name": map[string]interface{}{
-				"type": "text",
-				"fields": map[string]interface{}{
-					"keyword": map[string]string{"type": "keyword"},
-				},
-			},
-			"preferred_name": map[string]interface{}{"type": "text"},
-			"email":          map[string]interface{}{"type": "text"},
-			"phone":          map[string]string{"type": "keyword"},
-			"preferences":    map[string]interface{}{"type": "text"},
-			"notes":          map[string]interface{}{"type": "text"},
-			"floor":          map[string]string{"type": "integer"},
-			"room_number":    map[string]string{"type": "integer"},
-			"group_size":     map[string]string{"type": "integer"},
-			"booking_status": map[string]string{"type": "keyword"},
-			"arrival_date":   map[string]interface{}{"type": "date", "format": "strict_date_optional_time"},
-			"departure_date": map[string]interface{}{"type": "date", "format": "strict_date_optional_time"},
-		},
-	},
-}
-
 // EnsureGuestsIndex creates the guests index with its mapping if it doesn't already exist.
 func EnsureGuestsIndex(ctx context.Context, client *opensearch.Client) error {
 	res, err := opensearchapi.IndicesExistsRequest{Index: []string{GuestsIndex}}.Do(ctx, client)
@@ -83,7 +43,7 @@ func EnsureGuestsIndex(ctx context.Context, client *opensearch.Client) error {
 		return nil
 	}
 
-	body, err := json.Marshal(guestsIndexMapping)
+	body, err := json.Marshal(GuestsIndexMapping)
 	if err != nil {
 		return err
 	}
