109cd71ac4806bb2dbc2002afdf2ab4a42ced276
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-04-14 20:55:04 -0400

    lint

--- clients/web/src/components/guests/GuestQuickListTable.tsx
+++ clients/web/src/components/guests/GuestQuickListTable.tsx
@@ -74,7 +74,7 @@ export function GuestQuickListTable({
             <button
               key={guest.id}
               type="button"
-              onClick={() => onGuestClick(guest.id ?? "")}
+              onClick={() => onGuestClick(guest.id)}
               className="grid w-full grid-cols-[auto_5fr_5fr_2fr_2fr_2fr] items-center gap-[1vw] border-b border-black px-[1vw] py-[1vh] text-left last:border-b-0 hover:bg-neutral-50"
             >
               {avatarPill()}
