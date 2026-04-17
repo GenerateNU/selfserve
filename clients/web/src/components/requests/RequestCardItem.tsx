f4c24246417b395175b6ec27be6a4ee5a0dab0e3
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-04-17 04:13:51 -0400

    lint + format

--- clients/web/src/components/requests/RequestCardItem.tsx
+++ clients/web/src/components/requests/RequestCardItem.tsx
@@ -79,7 +79,7 @@ export function RequestCardItem({ request, onClick }: RequestCardItemProps) {
           {...listeners}
           onPointerDown={(e) => {
             if (e.button !== 0) return;
-            listeners.onPointerDown?.(e);
+            listeners.onPointerDown(e);
           }}
         >
           <RequestCard
