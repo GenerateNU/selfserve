3daf346ec9b8cb7c2ee04fee0ed2f442ed0bcf8a
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-04-16 20:06:39 -0400

    padding at bottom of page

--- clients/web/src/components/rooms/RoomsList.tsx
+++ clients/web/src/components/rooms/RoomsList.tsx
@@ -34,7 +34,7 @@ export function RoomsList({
   return (
     <section className="flex-1 min-h-0 flex flex-col overflow-hidden py-4">
       <nav className="flex-1 min-h-0">
-        <ul className="flex flex-col gap-4 h-full overflow-y-auto min-h-0 [&::-webkit-scrollbar]:hidden">
+        <ul className="flex flex-col gap-4 h-full overflow-y-auto min-h-0 pb-24 [&::-webkit-scrollbar]:hidden">
           {sortedRooms.map((room) => (
             <li key={room.room_number} className="min-w-0">
               <RoomCard
